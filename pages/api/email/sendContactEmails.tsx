24aa19893bbccbf5820927d822c1051d2b5bb7d2
Jon-Louis Dalton <jldalton12@gmail.com>
2022-06-22 23:40:33 +0200

    Removed try statement

--- pages/api/email/sendContactEmails.tsx
+++ pages/api/email/sendContactEmails.tsx
@@ -36,77 +36,71 @@ export default async function sendContactEmail(
 
   // Second we verify the reCAPTCHA token
   const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRETKEY}&response=${token}`;
-  try {
-    const reCAPTCHAResponse = await fetch(verifyUrl, { method: "POST" });
-    const reCAPTCHAJSON = await reCAPTCHAResponse.json();
-    if (reCAPTCHAJSON.score >= 0.9) {
-      // Third we send an email to support
-      const mailgun = new Mailgun(formData);
-      const client = mailgun.client({
-        username: "api",
-        key: "11cca92bdbb4cfa2bcac1dde2e6509b0-0be3b63b-70b4b404",
+  const reCAPTCHAResponse = await fetch(verifyUrl, { method: "POST" });
+  const reCAPTCHAJSON = await reCAPTCHAResponse.json();
+  if (reCAPTCHAJSON.score >= 0.9) {
+    // Third we send an email to support
+    const mailgun = new Mailgun(formData);
+    const client = mailgun.client({
+      username: "api",
+      key: "11cca92bdbb4cfa2bcac1dde2e6509b0-0be3b63b-70b4b404",
+    });
+
+    let emailTemplateSource = fs.readFileSync(
+      "./emailTemplates/provideAssistance.hbs",
+      "utf8"
+    );
+    let template = handlebars.compile(emailTemplateSource);
+    let htmlToSend = template({
+      name: name,
+      email: email,
+      phone: phone,
+      message: message,
+      year: new Date().getFullYear(),
+    });
+
+    client.messages
+      .create("occomy.com", {
+        from: "noreply@occomy.com",
+        to: "support@occomy.com",
+        subject: "Provide Assistance",
+        html: htmlToSend,
+        inline: {
+          data: fs.createReadStream("public/images/logo.png"),
+          filename: "logo.png",
+        },
+      })
+      .then(() => {
+        // Fourth we send an email to the cutomer
+        emailTemplateSource = fs.readFileSync(
+          "./emailTemplates/receivedAssistanceRequest.hbs",
+          "utf8"
+        );
+        template = handlebars.compile(emailTemplateSource);
+        htmlToSend = template({ name: name, year: new Date().getFullYear() });
+
+        client.messages
+          .create("occomy.com", {
+            from: "noreply@occomy.com",
+            to: email,
+            subject: "Request Received",
+            html: htmlToSend,
+            inline: {
+              data: fs.createReadStream("public/images/logo.png"),
+              filename: "logo.png",
+            },
+          })
+          .then(() => {
+            response.status(200).json({ status: "Success" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
       });
-
-      let emailTemplateSource = fs.readFileSync(
-        "./emailTemplates/provideAssistance.hbs",
-        "utf8"
-      );
-      let template = handlebars.compile(emailTemplateSource);
-      let htmlToSend = template({
-        name: name,
-        email: email,
-        phone: phone,
-        message: message,
-        year: new Date().getFullYear(),
-      });
-
-      client.messages
-        .create("occomy.com", {
-          from: "noreply@occomy.com",
-          to: "support@occomy.com",
-          subject: "Provide Assistance",
-          html: htmlToSend,
-          inline: {
-            data: fs.createReadStream("public/images/logo.png"),
-            filename: "logo.png",
-          },
-        })
-        .then((res) => {
-          console.log(res);
-          // Fourth we send an email to the cutomer
-          emailTemplateSource = fs.readFileSync(
-            "./emailTemplates/receivedAssistanceRequest.hbs",
-            "utf8"
-          );
-          template = handlebars.compile(emailTemplateSource);
-          htmlToSend = template({ name: name, year: new Date().getFullYear() });
-
-          client.messages
-            .create("occomy.com", {
-              from: "noreply@occomy.com",
-              to: email,
-              subject: "Request Received",
-              html: htmlToSend,
-              inline: {
-                data: fs.createReadStream("public/images/logo.png"),
-                filename: "logo.png",
-              },
-            })
-            .then((res) => {
-              console.log(res);
-              response.status(200).json({ status: "Success" });
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else {
-      response.status(200).json({ status: "Failed" });
-    }
-  } catch (e) {
-    response.status(200).json({ status: e });
+  } else {
+    response.status(200).json({ status: "Failed" });
   }
 }
