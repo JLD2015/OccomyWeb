f174d13c5dbaa6b23cd7a3df47bcd81a7de3c538
Jon-Louis Dalton <jldalton12@gmail.com>
2022-06-22 21:42:42 +0200

    Fixed links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -62,7 +62,7 @@ export default function Footer() {
               </Typography>
             </a>
           </Link>
-          <Link href="/contact">
+          <Link href="/Contact">
             <a>
               <Typography
                 sx={{
