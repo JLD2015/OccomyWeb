f6c70c7a57cc9961b4c8df4bb24ac7d52bcd48ca
Jon-Louis Dalton <jldalton12@gmail.com>
2022-07-19 18:10:16 +0200

    Some more testing

--- components/payment/PaymentForm.tsx
+++ components/payment/PaymentForm.tsx
@@ -150,8 +150,8 @@ export default function PaymentForm() {
 
           // Redirect back to the merchant's website
           setTimeout(function () {
-            // const redirectString = `${redirectURL}?status=declined`
-            const redirectString = `https://google.com?status=declined`;
+            const redirectString = `${redirectURL}?status=declined`;
+            console.log(redirectString);
             router.replace(redirectString);
             localStorage.clear();
           }, 5000);
