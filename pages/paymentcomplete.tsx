87e2e2ba393648344667edc1d2b7924f70ef79ec
Jon-Louis Dalton <jldalton12@gmail.com>
2022-07-19 14:57:01 +0200

    Had to add payment complete page

---
+++ pages/paymentcomplete.tsx
@@ -0,0 +1,34 @@
+import { Grid } from "@mui/material";
+import Footer from "../components/Footer";
+import Head from "next/head";
+import PaymentForm from "../components/payment/PaymentForm";
+
+export default function Payment() {
+  // <========== Body ==========>
+  return (
+    <>
+      {/* Page header */}
+      <Head>
+        <title>Occomy Payment</title>
+      </Head>
+
+      {/* Body */}
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        height={"100vh"}
+        sx={{
+          backgroundColor: "#f5f5f5",
+        }}
+      >
+        <PaymentForm />
+      </Grid>
+      {/* End body */}
+      {/* Footer */}
+      <Footer />
+      {/* End footer */}
+    </>
+  );
+}
