2db0c6b1f677cf31db6c1ccdf6b512a12f059bbc
Jon-Louis Dalton <jldalton12@gmail.com>
2022-08-09 22:13:33 +0200

    Changed vcard api

--- pages/api/xmpp/updatevcard.ts
+++ pages/api/xmpp/updatevcard.ts
@@ -1,65 +1,100 @@
 import { NextApiRequest, NextApiResponse } from "next";
 const crypto = require("crypto");
-import { client, xml, jid } from "@xmpp/client";
-const debug = require("@xmpp/debug");
+import { client, xml } from "@xmpp/client";
+import formidable from "formidable";
+import { readFileSync } from "fs";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default function approveTransaction(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  // First we need to get the post details
-  const body = request.body;
+  // First we need to get the POST details
+  const form = new formidable.IncomingForm();
+  form.parse(request, async (err, fields, files) => {
+    if (err) {
+      return response.status(400).json({ status: "Something went wrong" });
+    }
 
-  if (!body.jid) {
-    return response.status(400).json({ status: "Please provide a valid jid" });
-  }
-  const jidfull = body.jid;
-  const jid = body.jid.split("@")[0];
+    if (!files.picture) {
+      return response
+        .status(400)
+        .json({ status: "Please provide a profile picture" });
+    }
+    const picturepath = files.picture.filepath;
 
-  if (!body.jidpassword) {
-    return response
-      .status(400)
-      .json({ status: "Please provide a valid jid password" });
-  }
-  const jidpassword = body.jidpassword;
+    if (!fields.jid) {
+      return response
+        .status(400)
+        .json({ status: "Please provide a valid jid" });
+    }
+    const jidfull = fields.jid;
+    const jid = fields.jid.split("@")[0];
 
-  if (!body.name) {
-    return response.status(400).json({ status: "Please provide a valid name" });
-  }
-  const name = body.name;
+    if (!fields.jidpassword) {
+      return response
+        .status(400)
+        .json({ status: "Please provide a valid jid password" });
+    }
+    const jidpassword = fields.jidpassword;
 
-  // Create XMPP client
-  const xmpp = client({
-    service: "xmpp://xmpp.occomy.com:5222",
-    username: jid,
-    password: jidpassword,
-  });
+    if (!fields.name) {
+      return response
+        .status(400)
+        .json({ status: "Please provide a valid name" });
+    }
+    const name = fields.name;
 
-  // Monitor when we go online
-  xmpp.on("online", async (address) => {
-    console.log("Stream: Online");
-
-    // Update the user's VCard once we are online
-    const iq = xml(
-      "iq",
-      { id: crypto.randomUUID(), type: "set" },
-      xml(
-        "vCard",
-        { xmlns: "vcard-temp" },
-        xml("FN", {}, name),
-        xml("JABBERID", {}, jidfull)
-      )
-    );
-    await xmpp.send(iq);
-
-    // Stop the connection
-    xmpp.stop();
-
-    // Return completion to browser
-    return response.status(200).json({
-      status: "success",
+    // Second we create the XMPP client
+    const xmpp = client({
+      service: "xmpp://xmpp.occomy.com:5222",
+      username: jid,
+      password: jidpassword,
     });
-  });
 
-  xmpp.start().catch(console.error);
+    // Third we update the user's vCard when the XMPP connection goes live
+    // Monitor when we go online
+    xmpp.on("online", async (address) => {
+      console.log("Stream: Online");
+
+      // Get the base64 version of the image file
+      var bitmap = readFileSync(picturepath);
+      var base64picture = Buffer.from(bitmap).toString("base64");
+
+      // Update the user's VCard once we are online
+      const iq = xml(
+        "iq",
+        { id: crypto.randomUUID(), type: "set" },
+        xml(
+          "vCard",
+          { xmlns: "vcard-temp" },
+          xml("FN", {}, name),
+          xml("JABBERID", {}, jidfull),
+          xml(
+            "PHOTO",
+            {},
+            xml("TYPE", {}, "image/jpeg"),
+            xml("BINVAL", {}, base64picture)
+          )
+        )
+      );
+      await xmpp.send(iq);
+
+      // Stop the connection
+      xmpp.stop();
+
+      // Return completion to browser
+      return response.status(200).json({
+        status: "success",
+      });
+    });
+
+    // Fourth we start the XMPP connection
+    xmpp.start().catch(console.error);
+  });
 }
