1b50397fe499acde30038be7f2524a033defbdab
Jon-Louis Dalton <jldalton12@gmail.com>
2022-08-23 15:36:46 +0200

    Fixed messaging transaction

--- pages/api/messaging/sendfundsmessage.tsx
+++ pages/api/messaging/sendfundsmessage.tsx
@@ -147,73 +147,7 @@ export default function processWithdrawal(
               }
             );
 
-            // Upload the message to the server
-            const messageIdentifier = uuid();
-            transaction.set(
-              admin
-                .firestore()
-                .collection("newMessages")
-                .doc(messageIdentifier),
-              {
-                date: admin.firestore.FieldValue.serverTimestamp(),
-                fundsTransfer: true,
-                message: `R${Number(amount).toFixed(2)}`,
-                read: false,
-                received: false,
-                receiverID: recipientid,
-                senderID: uid,
-                senderName: senderData.name,
-                senderProfilePictureUrl: senderData.profilePhotoUrl,
-              }
-            );
-
-            // Send notifications to everybody
-
-            // Recipient
-            for (const token of recipientData.notificationTokens) {
-              sendNotification(
-                token,
-                "Received Payment",
-                `Received payment of R${Number(amount).toFixed(2)} from ${
-                  senderData.name
-                }`,
-                function (status) {
-                  console.log(status);
-                }
-              );
-            }
-
-            addNotification(
-              recipientDoc.id,
-              `Received: Received payment of R${Number(amount).toFixed(
-                2
-              )} from ${senderData.name}`
-            );
-
-            // Sender
-            for (const token of senderData.notificationTokens) {
-              sendNotification(
-                token,
-                "Made Payment",
-                `Made payment of R${Number(amount).toFixed(2)} to ${
-                  recipientData.name
-                }`,
-                function (status) {
-                  console.log(status);
-                }
-              );
-            }
-
-            addNotification(
-              senderDoc.id,
-              `Paid: Made payment of R${Number(amount).toFixed(2)} to ${
-                recipientData.name
-              }`
-            );
-
-            response
-              .status(200)
-              .json({ status: "Success", messageID: messageIdentifier });
+            response.status(200).json({ status: "Success" });
 
             // Still need to record an actual chat message
           } else {
